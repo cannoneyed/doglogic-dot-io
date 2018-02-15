372466649cd5d84efa9a6a53a293a5588bb24a54
Andy Coenen <andrew.coenen@gmail.com>
2018-02-14 20:35:24 -0800

    Update all

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -29,32 +29,47 @@ export default class App extends Component {
 
     const glitch = () => glitchStr.substring(0, this.props.dots * 2)
 
+    const src = 'https://www.youtube.com/embed/VrEZeSQ9VWY?rel=0&amp;controls=0&amp;showinfo=0'
+
     return (
-      <PageContainer>
-        <Row>
-          <Hash colorIndex={2}>0418</Hash>
-          <Prompt>/~dog_logic: </Prompt>
-          <Line>higher order functions{dots()}</Line>
-        </Row>
-        <Row>
-          <Line>
-            {
-              '> - █̿̿̿̿̿̿̿̿̿█̿̿̿̿̿̿̿̿█̿̿̿̿̿̿̿█̿̿̿̿̿̿█̿̿̿̿̿█̿̿̿̿█̿̿̿█̿̿█̿█͇█͇͇█͇͇͇█͇͇͇͇█͇͇͇͇͇█͇͇͇͇͇͇█͇͇͇͇͇͇͇█͇͇͇͇͇͇͇͇█͇͇͇͇͇͇͇͇͇'
-            }
-            {glitch()}
-          </Line>
-        </Row>
-        <Row />
-        <Row mt={0.1}>
-          <Console />
-        </Row>
-      </PageContainer>
+      <PageWrapper>
+        <PageContainer>
+          <Row>
+            <VideoSection>
+              <iframe width="100%" src={src} frameBorder="0" allowFullScreen />
+            </VideoSection>
+          </Row>
+          <Row>
+            <Hash colorIndex={2}>0418</Hash>
+            <Prompt>/~dog_logic: </Prompt>
+            <Line>higher order functions{dots()}</Line>
+          </Row>
+          <Row>
+            <Line>
+              {
+                '> - █̿̿̿̿̿̿̿̿̿█̿̿̿̿̿̿̿̿█̿̿̿̿̿̿̿█̿̿̿̿̿̿█̿̿̿̿̿█̿̿̿̿█̿̿̿█̿̿█̿█͇█͇͇█͇͇͇█͇͇͇͇█͇͇͇͇͇█͇͇͇͇͇͇█͇͇͇͇͇͇͇█͇͇͇͇͇͇͇͇█͇͇͇͇͇͇͇͇͇'
+              }
+              {glitch()}
+            </Line>
+          </Row>
+          <Row />
+          <Row mt={0.1}>
+            <Console />
+          </Row>
+        </PageContainer>
+      </PageWrapper>
     )
   }
 }
-
+const PageWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 const PageContainer = styled.div`
   width: 100%;
+  max-width: 800px;
   height: 100%;
 `
 
@@ -85,4 +100,18 @@ const Line = styled.span`
 
 const Hash = styled.span`
   color: ${props => colors[props.colorIndex]};
+`
+
+const VideoSection = styled.div`
+  position: relative;
+  padding-bottom: 56.25%; /* 16:9 */
+  padding-top: 25px;
+  height: 0;
+  > iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
 `
--- src/components/Console/index.js
+++ src/components/Console/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import MediaQuery from 'react-responsive'
 import styled from 'styled-components'
 import Console from 'react-console-component'
 import isEmail from 'isemail'
@@ -24,11 +25,12 @@ const getErrorMsg = (err, data, email) => {
 export default class ConsoleComponent extends Component {
   componentDidMount() {
     this.consoleElement = ReactDOM.findDOMNode(this.console)
-    const textarea = this.consoleElement.querySelector('textarea')
-    textarea.setAttribute('type', 'email')
-    console.log(textarea)
-
-    setTimeout(this.toggleCursor, 500)
+    if (this.consoleElement) {
+      const textarea = this.consoleElement.querySelector('textarea')
+      setTimeout(this.toggleCursor, 500)
+      this.console.focus()
+      console.log(this.console)
+    }
   }
 
   state = {
@@ -37,15 +39,18 @@ export default class ConsoleComponent extends Component {
 
   toggleCursor = (on = true) => {
     this.cursorElement = this.consoleElement.querySelector('.react-console-cursor')
-    const isFocused = this.consoleElement.classList.contains('react-console-focus')
     if (this.cursorElement) {
-      this.cursorElement.style.backgroundColor = on && isFocused ? colors[0] : colors[13]
+      this.cursorElement.style.backgroundColor = on ? colors[0] : colors[13]
       this.cursorElement.style.opacity = 0.5
     }
     setTimeout(() => this.toggleCursor(!on), 500)
   }
 
-  echo = text => {
+  handleConsoleSubmit = text => {
+    if (!text) {
+      return
+    }
+
     if (this.state.hasRegistered) {
       this.console.log(text)
       this.console.return()
@@ -73,20 +78,51 @@ export default class ConsoleComponent extends Component {
     }
   }
 
+  handleSubmit = e => {
+    if (e) {
+      e.preventDefault()
+    }
+    const text = this.input.value
+    this.input.value = ''
+    setTimeout(() => {
+      this.console.acceptLine()
+    }, 0)
+  }
+
+  handleInputChange = () => {
+    const text = this.input.value
+    const textarea = this.consoleElement.querySelector('textarea')
+    textarea.value = text
+    this.console.change()
+  }
+
   render() {
     const welcomeMessage =
       '█͇█͇͇█͇͇͇█͇͇͇͇█͇͇͇͇͇█͇͇͇͇͇͇█͇͇͇͇͇͇͇█͇͇͇͇͇͇͇͇█͇͇͇͇͇͇͇͇͇\ndebut album release 0418\nenter email for free download, stems, and more'
     return (
       <ConsoleWrapper>
-        <Console
-          ref={ref => {
-            this.console = ref
-          }}
-          handler={this.echo}
-          autofocus={true}
-          promptLabel={'> '}
-          welcomeMessage={welcomeMessage}
-        />
+        <MediaQuery minDeviceWidth={1224}>
+          <Console
+            ref={ref => {
+              this.console = ref
+            }}
+            handler={this.handleConsoleSubmit}
+            autofocus={true}
+            promptLabel={'> '}
+            welcomeMessage={welcomeMessage}
+          />
+        </MediaQuery>
+        <BottomSection>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              onChange={this.handleInputChange}
+              ref={ref => (this.input = ref)}
+              placeholder="enter email"
+              type="email"
+            />
+            <button onClick={this.handleSubmit}>submit</button>
+          </form>
+        </BottomSection>
       </ConsoleWrapper>
     )
   }
@@ -98,5 +134,44 @@ const ConsoleWrapper = styled.div`
   }
   & .react-console-welcome {
     white-space: pre-line;
+  }
+`
+
+const BottomSection = styled.div`
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+  max-width: 800px;
+
+  > form {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding-bottom: 1rem;
+
+    > input {
+      background-color: ${colors[0]};
+      font-family: 'Ubuntu Mono', monospace;
+      height: 1.2rem;
+      font-size: 1.2rem;
+      width: 70%;
+    }
+
+    > input:focus {
+      outline: none;
+    }
+
+    > button {
+      border: 1px solid ${colors[0]};
+      background-color: black;
+      color: ${colors[0]};
+      font-family: 'Ubuntu Mono', monospace;
+      height: 1.5rem;
+      font-size: 1.2rem;
+      padding: 0;
+      width: 20%;
+    }
   }
 `

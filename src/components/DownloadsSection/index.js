2fdde2c946de6ea4e7e4731ba2a3a9de758763ab
Andy Coenen <andycoenen@andycoenen-macbookpro.roam.corp.google.com>
2018-04-19 11:17:40 -0700

    Add first batch of stems links

--- src/components/DownloadsSection/index.js
+++ src/components/DownloadsSection/index.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import {range} from 'lodash'
 
 const MP3_HREF = 'https://s3-us-west-2.amazonaws.com/higher-order-functions.clips/downloads/dog_logic_higher_order_functions_mp3.zip'
 const AAC_HREF = 'https://s3-us-west-2.amazonaws.com/higher-order-functions.clips/downloads/dog_logic_higher_order_functions_aac.zip'
 const WAV_HREF = 'https://s3-us-west-2.amazonaws.com/higher-order-functions.clips/downloads/dog_logic_higher_order_functions_wav.zip'
+
+const stems = [{
+  index: '01',
+  track: 'higher order',
+  link: 'https://s3-us-west-2.amazonaws.com/higher-order-functions.clips/downloads/stems/01-higher_order(126).zip',
+  bpm: [126],
+  size: '408mb'
+}, {
+  index: '02',
+  track: 'heartbeats',
+  link: 'https://s3-us-west-2.amazonaws.com/higher-order-functions.clips/downloads/stems/02-heartbeats(124).zip',
+  bpm: [124],
+  size: '202mb'
+}, {
+  index: '03',
+  track: 'ants army',
+  link: 'https://s3-us-west-2.amazonaws.com/higher-order-functions.clips/downloads/stems/03-ants_army(128).zip',
+  bpm: [128],
+  size: '196mb',
+}, {
+  index: '04',
+  track: 'heartbeats',
+  link: 'https://s3-us-west-2.amazonaws.com/higher-order-functions.clips/downloads/stems/04-frog_song(128-120).zip',
+  bpm: [128, 120],
+  size: '269mb',
+}]
 
 export class DownloadsSection extends Component {
   renderAlbumLinks = () => {
@@ -16,31 +43,60 @@ export class DownloadsSection extends Component {
     )
   }
 
+  renderStemDownloadLink(stem) {
+    const {index, track, bpm, size} = stem
+    const text = `${index}: ${track}`
+    
+    const details = ` (${bpm.join('/')})bpm | ${size}`
+    return (
+      <DownloadLink>
+        <a href={stem.link}>{text}</a>
+        {details}
+      </DownloadLink>
+    )
+  }
+
   render() {
     return (
-      <div>
+      <DonwloadSection>
         <Header>ALBUM DOWNLOAD</Header>
         {this.renderAlbumLinks()}
         
         <Header>STEMS DOWNLOAD</Header>
-        <ComingSoon>coming soon...</ComingSoon>
-      </div>
+        <LinksWrapper>
+          {stems.map(this.renderStemDownloadLink)}
+        </LinksWrapper>
+        <ComingSoon>more coming soon...</ComingSoon>
+      </DonwloadSection>
     )
   }
 }
 
+const DonwloadSection = styled.div`
+  color: white;
+
+  a {
+    margin-right: 15px;
+    color: white;
+    display: block;
+  }
+`
+
 const ComingSoon = styled.span`
-    color: white;    
+    margin-top: 20px;
 `
 
 const Header = styled.div`
     margin: 10px 0;
 `
 
-const LinksWrapper = styled.span`
-  > a {
+const LinksWrapper = styled.div`
+  a {
     margin-right: 15px;
-    color: white;
     display: block;
   }
+  margin: 10px;
+`
+const DownloadLink = styled.span`
+  display: flex;
 `
